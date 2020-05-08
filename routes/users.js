ceaa645c8e22cac9c1e07b1600ade87912e06e34
akazmi30 <akazmi30@gatech.edu>
2020-05-08 11:06:42 -0700

    customized data model start

---
+++ routes/users.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+// Getting all subscribers
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+// Creating one subscriber
+router.post('/', async (req, res) => {
+  const user = new User({
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+    birthday: req.body.birthday,
+    locationTested: req.body.locationTested
+  })
+
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+// Getting one subscriber
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber)
+})
+
+// Updating one subscriber
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+
+  if (req.body.subscribedChannel != null) {
+    res.subscriber.subscribedChannel = req.body.subscribedChannel
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch (err){
+    res.status(400).json({ message: err.message })
+  }
+
+})
+// Deleting one subscriber
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove()
+    res.json({ message: 'Deleted This Subscriber' })
+  } catch(err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+
+// Middleware function for gettig subscriber object by ID
+async function getSubscriber(req, res, next) {
+  try {
+    subscriber = await Subscriber.findById(req.params.id)
+    if (subscriber == null) {
+      return res.status(404).json({ message: 'Cant find subscriber'})
+    }
+  } catch(err){
+    return res.status(500).json({ message: err.message })
+  }
+  
+  res.subscriber = subscriber
+  next()
+}
+
+module.exports = router 
